355cbae346d431e748a741d5f3e15c4efc6f26d5
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-07-02 16:15:49 -0500

    lab4

---
+++ Assignments/Lab4A/app.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const path=require('path')
+const user=require('./router/user')
+const product=require('./router/product')
+const error=require('./router/error')
+// const errorHandler=require('./router/errorHandler')
+// const pageNotFound=require('./router/notFound')
+let jsonParam=express.json()
+//req.qury
+//req.parema
+let urlEncoder=express.urlencoded({extended:true})
+//req.body
+const app=express()
+ app.use(urlEncoder)
+app.use(user)
+app.use(product)
+app.use(error)
+app.use((err ,req, res, next)=> { 
+    res.sendFile(path.join(__dirname,'./','views','500.html'))
+});
+app.use((req, res, next) => { 
+    res.status(404).sendFile(path.join(__dirname,'./','views','404.html'))
+})
+app.listen(3000,()=>{
+    console.log("listening on port 3000")
+})
